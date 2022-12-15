8ed196b0a6ed2b033a5ac4064c3822fdd9d1e84f
Sakshi <70653660+Sakshi887220@users.noreply.github.com>
2022-12-15 15:14:16 +0530

    Update main.py

--- main.py
+++ main.py
@@ -8,64 +8,30 @@ app = Flask(__name__)
 
 @app.route("/get-movie")
 def get_movie():
-    movie_data = {
-        "title": all_movies[0][19],
-        "poster_link": all_movies[0][27],
-        "release_date": all_movies[0][13] or "N/A",
-        "duration": all_movies[0][15],
-        "rating": all_movies[0][20],
-        "overview": all_movies[0][9]
-    }
-    return jsonify({
-        "data": movie_data,
-        "status": "success"
-    })
+   
 
 @app.route("/liked-movie", methods=["POST"])
 def liked_movie():
-    movie = all_movies[0]
-    liked_movies.append(movie)
-    all_movies.pop(0)
-    return jsonify({
-        "status": "success"
-    }), 201
+    
 
 @app.route("/unliked-movie", methods=["POST"])
 def unliked_movie():
-    movie = all_movies[0]
-    not_liked_movies.append(movie)
-    all_movies.pop(0)
-    return jsonify({
-        "status": "success"
-    }), 201
+    
 
 @app.route("/did-not-watch", methods=["POST"])
 def did_not_watch_view():
-    movie = all_movies[0]
-    did_not_watch.append(movie)
-    all_movies.pop(0)
-    return jsonify({
-        "status": "success"
-    }), 201
+    
 
 @app.route("/popular-movies")
 def popular_movies():
     movie_data = []
     for movie in output:
         _d = {
-            "title": movie[0],
-            "poster_link": movie[1],
-            "release_date": movie[2] or "N/A",
-            "duration": movie[3],
-            "rating": movie[4],
-            "overview": movie[5]
+           
         }
         movie_data.append(_d)
     return jsonify({
-        "data": movie_data,
-        "status": "success"
-    }), 200
-
+        
 @app.route("/recommended-movies")
 def recommended_movies():
     all_recommended = []
